458525cb851ff89ba9589644b881a1cf7ad592b7
Italo Js <italo.i@live.com>
2018-02-06 08:58:00 -0200

    first commit

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Neuron struct{
+	Samples [][]float64
+	Classes []float64
+	Weights []float64
+    TrainingRate float64
+}
+
+func stepFuction(sum float64)(result int){
+	if sum >= 1{
+		return 1
+	}
+	return 0
+}
+
+func calculateClass(sample []float64, weights []float64)(result int){
+	var sum float64
+	for i := range sample{
+		sum += sample[i]*weights[i]
+	}
+	return stepFuction(sum)
+}
+
+func train(n *Neuron){
+	errors := 1.0
+	times := 0
+	for errors >0 && times < 10000 {
+		errors = 0
+		times ++
+		for i, class := range (*n).Classes{
+			neuronResult := calculateClass((*n).Samples[i],(*n).Weights)
+			err := class - float64(neuronResult)
+			errors += err
+			for j, weight := range (*n).Weights{
+				(*n).Weights[j] = weight + ((*n).TrainingRate * (*n).Samples[i][j] * err)
+				fmt.Println("peso atualizado de ", weight, " para ", (*n).Weights[j])
+			}
+			fmt.Println("erros: ", err)
+		}
+	}
+}
+
+func main (){
+	n := Neuron{
+			Samples:[][]float64{{0,0},{0,1}, {1,0},{1,1}},
+			Classes:[]float64{0,1,1,0},
+			Weights:[]float64{0,0},
+			TrainingRate: 0.1}
+	train(&n)
+	for i, s := range n.Samples{
+		fmt.Println("peso: ", i ," classe: ", calculateClass(s,n.Weights))
+	}
+}
